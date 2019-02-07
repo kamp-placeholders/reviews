8fdf9a0e41e8949203879a2bf8312490cf056d2d
PlasmaIntec <alphaintec@gmail.com>
2019-02-06 17:22:28 -0800

    Implement individual reviews
    circle colors are generated based off ascii codes
    may need to implement buttons for helpfulness and reporting
    will work on setting up database

--- client/index.js
+++ client/index.js
@@ -1,3 +1,7 @@
-import Reviews from './components/reviews.jsx';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-window.Reviews = Reviews;
+import Reviews from './components/reviews.jsx';
+import sample from './sampleData.js';
+
+ReactDOM.render(<Reviews reviews={sample} />, document.getElementById('reviews'));
---
+++ client/sampleData.js
@@ -0,0 +1,50 @@
+var reviews = [
+{
+	'name': 'EricC',
+	'city': 'San Francisco',
+	'stars': 4,
+	'pastReviews': 2,
+	'isVIP': false,
+	'date': '1/26/2019',
+	'review': {
+		'post': `Food was all quite good, felt clean and balanced tastes. Bread however should have definitely been served warm.`,
+		'overall': 4,
+		'food': 4,
+		'service': 4,
+		'ambience': 4
+	}
+},
+{
+	'name': 'MVeeC',
+	'city': 'New York Area',
+	'stars': 5,
+	'pastReviews': 5,
+	'isVIP': true,
+	'date': '1/25/2019',
+	'review': {
+		'post': `Outstanding and consistently so.
+Garrett was a great server, oyster maven!`,
+		'overall': 5,
+		'food': 5,
+		'service': 5,
+		'ambience': 5
+	}
+},
+{
+	'name': 'lowercasetest',
+	'city': 'Mongolia',
+	'stars': 0,
+	'pastReviews': 15,
+	'isVIP': true,
+	'date': '1/2/2019',
+	'review': {
+		'post': `hello i only type in lowercase letters`,
+		'overall': 5,
+		'food': 3,
+		'service': 1,
+		'ambience': 1
+	}
+}
+];
+
+module.exports = reviews;
