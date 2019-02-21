974c3919bfcb477bb3071ea2bba1d2f697c270ca
PlasmaIntec <alphaintec@gmail.com>
2019-02-21 15:21:14 -0800

    Add config files

---
+++ server/db/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  host: 'reviews.ccm1cvmgiugc.us-east-2.rds.amazonaws.com',
+  port: '3306',
+  user: 'root',
+  password: 'password',
+  database: 'reviews'
+};
---
+++ server/foodparser.config.js
@@ -0,0 +1,3 @@
+var API_KEY = '6b607176f2msh4c8f9db9f2a6ac6p1f35c6jsn5ded9cb9b9ef';
+
+module.exports = API_KEY;
